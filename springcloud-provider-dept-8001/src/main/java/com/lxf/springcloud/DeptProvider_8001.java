0f40164777db38bfb9b15e154848dfc08d47d1dd
Hiiro <1142803933@qq.com>
2021-12-07 12:10:05 +0800

    2021.12.7 修改数据库链接信息

--- springcloud-provider-dept-8001/src/main/java/com/lxf/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/lxf/springcloud/DeptProvider_8001.java
@@ -2,6 +2,8 @@ package com.lxf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Hiiro
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class,args);
